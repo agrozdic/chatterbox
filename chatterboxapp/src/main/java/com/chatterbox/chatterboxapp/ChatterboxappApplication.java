98b9e2b8f40e1eb9847cbc78d949d61974ac0902
Aleksandar Grozdić <agrozdic08@gmail.com>
2023-05-20 22:07:38 +0200

    Modeli

--- chatterboxapp/src/main/java/com/chatterbox/chatterboxapp/ChatterboxappApplication.java
+++ chatterboxapp/src/main/java/com/chatterbox/chatterboxapp/ChatterboxappApplication.java
@@ -2,7 +2,11 @@ package com.chatterbox.chatterboxapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"com.chatterbox.model"}) //Here we give the root to our package containing Post and User Classes.
 @SpringBootApplication
 public class ChatterboxappApplication {
 
