e885ee8f5df31a9d864ecdf4053d8448150a940d
Aleksandar Grozdić <agrozdic08@gmail.com>
2023-05-20 18:40:13 +0200

    Inicijalizacija projekta

---
+++ chatterboxapp/src/main/java/com/chatterbox/chatterboxapp/ChatterboxappApplication.java
@@ -0,0 +1,13 @@
+package com.chatterbox.chatterboxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatterboxappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatterboxappApplication.class, args);
+	}
+
+}
---
+++ chatterboxapp/src/test/java/com/chatterbox/chatterboxapp/ChatterboxappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatterbox.chatterboxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatterboxappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
