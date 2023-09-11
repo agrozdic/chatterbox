f7690f9623e72bf89df140dc20b86be1b8f0b597
Aleksandar Grozdić <agrozdic08@gmail.com>
2023-09-11 19:43:07 +0200

    Autorizacija

--- chatterboxapp/angular-frontend/chatterbox/src/app/app-routing.module.ts
+++ chatterboxapp/angular-frontend/chatterbox/src/app/app-routing.module.ts
@@ -17,6 +17,9 @@ import { CreatePostComponent } from './feed/posts/create-post/create-post.compon
 import { CreateGroupComponent } from './feed/groups/create-group/create-group.component';
 import { CommentDetailsComponent } from './feed/posts/post-details/comments/comment-details/comment-details.component';
 import { CommentDeletedComponent } from './comments/comment-deleted/comment-deleted.component';
+import { FriendRequestsComponent } from './friend-requests/friend-requests.component';
+import { UsersComponent } from './users/users.component';
+import { SendFriendRequestsComponent } from './send-friend-requests/send-friend-requests.component';
   const routes: Routes = [
     {path: '',component: HomePageComponent,
   //  children: [  { path: 'login',component: LoginComponent }
@@ -26,16 +29,19 @@ import { CommentDeletedComponent } from './comments/comment-deleted/comment-dele
   {path: 'registrationsuccess',component: RegistrationSuccesfulComponent},
   { path: 'login',component: LoginComponent },
   { path: 'profile', component: UserProfileComponent, canActivate: [AuthGuard]},
-  { path: 'feed',component:FeedComponent},
-  {path: 'group-detail',component: GroupDetailComponent},
-  {path: 'group-reqest', component: GroupReqestComponent},
-  {path: 'success-deletion-group',component: SuccesDeletionGroupComponent},
-  {path: 'post-details', component: PostDetailsComponent},
-  {path: 'post-deleted', component: PostDeletedComponent},
-   {path: 'add-post', component: CreatePostComponent},
-   {path: 'add-group', component: CreateGroupComponent},
-   {path: 'comment-details', component: CommentDetailsComponent},
-  {path: 'comment-deleted', component: CommentDeletedComponent}
+  { path: 'feed',component:FeedComponent, canActivate: [AuthGuard]},
+  {path: 'group-detail',component: GroupDetailComponent, canActivate: [AuthGuard]},
+  {path: 'group-reqest', component: GroupReqestComponent, canActivate: [AuthGuard]},
+  {path: 'success-deletion-group',component: SuccesDeletionGroupComponent, canActivate: [AuthGuard]},
+  {path: 'post-details', component: PostDetailsComponent,canActivate: [AuthGuard]},
+  {path: 'post-deleted', component: PostDeletedComponent,canActivate: [AuthGuard]},
+   {path: 'add-post', component: CreatePostComponent,canActivate: [AuthGuard]},
+   {path: 'add-group', component: CreateGroupComponent,canActivate: [AuthGuard]},
+   {path: 'comment-details', component: CommentDetailsComponent,canActivate: [AuthGuard]},
+  {path: 'comment-deleted', component: CommentDeletedComponent,canActivate: [AuthGuard]},
+  {path: 'friend-requests', component: FriendRequestsComponent,canActivate: [AuthGuard]},
+  {path: 'friends', component: UsersComponent,canActivate: [AuthGuard]},
+  {path: 'users', component: SendFriendRequestsComponent,canActivate: [AuthGuard]}
 
   ];
 
